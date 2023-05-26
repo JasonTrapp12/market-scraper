2a1723077476f145e645a6258025f727ed448db7
JasonTrapp12 <jwt9552@g.rit.edu>
2023-05-26 13:41:03 -0400

    created scraper project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'facebook-scraper';
+  title = 'marketplace-scraper';
+  keyword = '';
+
+  
+  confirmKeyword(){
+    console.log(this.keyword);
+  }
 }
+
+
